7a50c29f7ac2488c00fee2ef244abe701a882a1a
DanielFryyEPN <daniel.freire01@epn.edu.ec>
2017-07-22 17:15:15 -0500

    mas funcionalidad

--- js/main.js
+++ js/main.js
@@ -1,20 +1,24 @@
-var images = [];
-for(var i = 1; i <= 8; i++) {
+let images = [];
+for(let i = 1; i <= 8; i++) {
+  images.push('assets/Images/' + i + '.jpg');
   images.push('assets/Images/' + i + '.jpg');
 }
-hola = function () {
-  console.log('ya hice algo mas nuevo');
-  var index = Math.floor(Math.random() * 9);
-  console.log(index);
+let hola = function () {
+  //$('img').attr('src');
+  console.log($('#img1').attr('src'));
 };
-$(document).ready(function(){
-  $('td').click(hola);
-  for(var i = 1; i <= 8; i++) {
-    var index1 = Math.floor(Math.random() * 9) + 1;
-    $('#img' + index1).attr('src', images[i-1]);
+let index = [];
+for(let k = 1; k <= 16; k++){
+  let rand = Math.floor(Math.random() * 16) + 1;
+  if((index.find(function (value) {return value === rand;})) === undefined) {
+    index.push(rand);
+  } else {
+    k--;
   }
-  for(i; i <= 16; i++) {
-    var index2 = Math.floor(Math.random() * 9) + 8;
-    $('#img' + index2).attr('src', images[i-9]);
+}
+$(document).ready(function(){
+  $('#img1').click(hola);
+  for (let i = 0; i < 16; i++) {
+    $('#img' + index[i]).attr('src', images[i]);
   }
 });
