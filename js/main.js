e5b8d8ffeed32b0b17e86b226f503543493c0aa5
DanielFryyEPN <daniel.freire01@epn.edu.ec>
2017-07-22 23:07:11 -0500

    tratando de hacer el emparejamiento

--- js/main.js
+++ js/main.js
@@ -3,9 +3,34 @@ for(let i = 1; i <= 8; i++) {
   images.push('assets/Images/' + i + '.jpg');
   images.push('assets/Images/' + i + '.jpg');
 }
+let try1 = '';
+let try2 = '';
+let contador = 0;
+let iguales = false;
 let hola = function () {
-  //$('img').attr('src');
-  console.log($('#img1').attr('src'));
+  if(contador < 2 && $(this).children('img').hasClass('hide') === true) {
+    $(this).children('img').addClass('show');
+    contador++;
+    if (contador === 1) {
+      try1 = $(this).children('img').attr('src');
+    } else {
+      try2 = $(this).children('img').attr('src');
+      if (try1 === try2) {
+        let id = $(this).children('img').attr('id');
+        
+        $('#' + id).a
+        console.log('html =>', id);
+        $('td').children('img').attr('src', try1);
+        console.log('try1:', try1, 'try2:', try2);
+        iguales = true;
+        console.log(iguales);
+      } else {
+        iguales = false;
+        console.log(iguales);
+      }
+      contador = 0;
+    }
+  }
 };
 let index = [];
 for(let k = 1; k <= 16; k++){
@@ -17,8 +42,8 @@ for(let k = 1; k <= 16; k++){
   }
 }
 $(document).ready(function(){
-  $('#img1').click(hola);
   for (let i = 0; i < 16; i++) {
     $('#img' + index[i]).attr('src', images[i]);
   }
+  $('td').click(hola);
 });
